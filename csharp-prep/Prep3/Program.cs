da675d803c873aa4353e5ec7033cb7b636764e13
daddymac77 <cri22004@byui.edu>
2023-09-18 09:34:38 +0800

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
-}
+       Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+
+        int guessedNumber = -1;
+        while (magicNumber != guessedNumber);
+        {
+       Console.Write("What is your guess? ");
+        guessedNumber = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guessedNumber)
+        {
+            Console.Write("Higher!");
+        }
+        
+        else if (magicNumber < guessedNumber)
+        {
+            Console.Write("Lower!");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
+
+        
+            
+        
+        
+            
+        }
+        }
+        }
+
+        
+
+        
+    
+    
+
